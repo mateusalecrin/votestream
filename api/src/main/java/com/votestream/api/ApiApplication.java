32b91326e9825609df0ffb08fd72b7292b76c9be
mateusalecrin <mateus.alecrin@hotmail.com>
2026-08-16 22:44:36 -0300

    chore: configura propriedades da aplicação (datasource, JPA, Jackson, locale)

--- api/src/main/java/com/votestream/api/ApiApplication.java
+++ api/src/main/java/com/votestream/api/ApiApplication.java
@@ -3,10 +3,13 @@ package com.votestream.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Locale;
+
 @SpringBootApplication
 public class ApiApplication {
 
 	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
 		SpringApplication.run(ApiApplication.class, args);
 	}
 
