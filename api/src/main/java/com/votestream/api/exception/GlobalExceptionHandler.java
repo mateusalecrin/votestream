10af478f0468da583bca0eb4c2e93d9022b2b1d6
mateusalecrin <mateus.alecrin@hotmail.com>
2026-08-21 19:25:54 -0300

    fix(exception): corrige import do Jackson 3 no GlobalExceptionHandler

--- api/src/main/java/com/votestream/api/exception/GlobalExceptionHandler.java
+++ api/src/main/java/com/votestream/api/exception/GlobalExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.votestream.api.exception;
 
-import com.fasterxml.jackson.databind.exc.UnrecognizedPropertyException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
@@ -8,6 +7,7 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import tools.jackson.databind.exc.UnrecognizedPropertyException;
 
 import java.time.LocalDateTime;
 import java.util.Map;
