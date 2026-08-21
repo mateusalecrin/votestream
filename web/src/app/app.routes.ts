d14fec3083ff652563928b67acfceaa11d779ca3
mateusalecrin <mateus.alecrin@hotmail.com>
2026-08-21 19:25:59 -0300

    chore(web): configura HttpClient, rotas e remove template padrão do Angular

--- web/src/app/app.routes.ts
+++ web/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'polls', pathMatch: 'full' },
+  {
+    path: 'polls',
+    loadComponent: () => import('./features/polls/poll-list/poll-list').then((m) => m.PollList),
+  },
+  {
+    path: 'polls/new',
+    loadComponent: () => import('./features/polls/poll-create/poll-create').then((m) => m.PollCreate),
+  },
+  {
+    path: 'polls/:id',
+    loadComponent: () => import('./features/polls/poll-detail/poll-detail').then((m) => m.PollDetail),
+  },
+];
